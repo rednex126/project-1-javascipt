e8a995f044adfcdf73ad5640523e1cb5df46b431
rednex126 <pavlenkou318@gmail.com>
2024-10-27 10:20:12 +0100

    урок1+2

---
+++ Html/js/cons/cons1/main.js
@@ -0,0 +1,21 @@
+console.log('test');
+console.log('--------');
+
+
+let num = '42.456432';
+console.log(typeof num);
+console.log(typeof parseInt(num));
+
+
+let name = 'solo';
+let surName = 'Ollo';
+console.log(`${name} ${surName}`);  //інтерполяція ----->>>>  ``
+
+
+let xxx = 111;
+xxx = 'Ivan';
+console.log(xxx);
+
+let prmt = prompt('enter your name');
+console.log( prmt);
+
---
+++ Html/js/lessons/l2/l2.js
@@ -0,0 +1,137 @@
+// let dogMas1 = [
+//     { id:1, name: 'kokos', breed: 'spaniel', age:3},
+//     { id:2, name: 'ananas', breed: 'staff', age: 4},
+//     { id:3, name: 'kit', breed: 'pes', age:2},
+//
+// ];
+//
+// // console.log(dogMas1.length)
+// // let dog1 = dogMas1 [1];
+// // console.log(dog1);
+//
+// let dogMas2 = [
+//     { id:1, name: 'banana', breed: 'taxa', age:5},
+//     { id:2, name: 'avokado', breed: 'pug', age: 6},
+//     { id:3, name: 'popil', breed: 'dog', age: 1},
+//
+// ];
+//
+// let cats1 = [
+//     { id:11, name: 'carapka', breed: 'gemini', age:5},
+//     { id:22, name: 'batton', breed: 'pers', age: 6},
+//
+//
+// ];
+//
+//
+//
+// let pets = [
+//     dogMas1,
+//     dogMas2,
+//     cats1
+// ]
+//
+// console.log(pets.length);
+// console.log(pets);
+// console.log(pets[0][0]);
+// console.log(pets[1][2].breed);
+
+
+// let devs = [
+//     {name:'vasya', skills:['html', 'js', 'javascript']},
+//     {name:'petya', skills:['html', 'js', 'nodejs']},
+//     {name:'anna', skills:['html', 'js', ]},
+// ];
+//
+// console.log(devs);
+// console.log(devs[0]);
+// console.log(devs[0].skills);
+// console.log(devs[1].skills[0]);
+// console.log(devs[2].skills[12]);
+
+
+//
+// let obj = {};
+// console.log(obj);
+//
+// obj.name = 'igor';
+// console.log(obj);
+//
+// delete obj.name;
+// console.log(obj);
+
+
+
+//
+// let p = 0;
+// let p2 = p;
+// console.log(p2);
+//
+// p2 = p2 + 100;
+// console.log(p, p2);
+//
+
+
+// let obj1 = {id:1};
+// let obj2 = obj1;
+// console.log(obj2);
+//
+// obj2.name = 'sadwwwss';
+// console.log(obj2);
+//
+// console.log(obj1);
+//
+// console.log(typeof []);
+// console.log(typeof {});
+
+
+
+// && строгий оператор , || не строгий оператор, достаточно 1 правильно ответа
+
+
+
+// let age = 16;
+// if(age > 0 && age <= 18) {
+//     document.write(<h2>Hello young User</h2>);
+// } else if (age >18 && age <= 35) {
+//     document.write(<h2>Hello Adult User</h2>);
+// } else if (age >35 && age <= 60) {
+//
+//         document.write(<h2>Old</h2>);
+//
+//     } else {
+//     document.write(<h2>******</h2>);
+// }
+//
+//
+//
+//
+
+// let user = {name: 'oloko', age: 55};
+// switch (user.age) {
+//     case 18:
+//         console.log('ypung');
+//         break;
+//     case 35:
+//         console.log('old')
+//         if(true) {
+//         }else {
+//          switch()
+//         }
+//         break;
+//     case 40:
+//             console.log('no');
+//             break;
+//
+//             default:
+//                 console.log('olololololololo');
+//
+// }
+
+
+// if - необхідність перевірки на 1 або 2 значення, прості перевірки, діапозонові перевірки
+// switch - множинні перевірки але без діапазонів
+// breal -
+
+//else if - для варіативниз перевірок
+// світч для точнових перевірок
