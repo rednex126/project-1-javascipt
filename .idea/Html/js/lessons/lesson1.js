a7e6941101cd98e50e4d4e4aa01fcf06f185ae46
rednex126 <pavlenkou318@gmail.com>
2024-10-27 10:18:25 +0100

    something

---
+++ .idea/Html/js/lessons/lesson1.js
@@ -0,0 +1,53 @@
+// var a ;
+//     a = 'local';
+//     a = 15;
+//     a = true;
+//
+//     var b = 3;
+//     b   = true;
+//     b   = 'string';
+//
+
+// let a = 'okten';
+// a = 1000;
+// console.log(a);
+
+
+// console.log('okten');
+// console.log('okten');
+// console.log('okten');
+// console.log('okten');
+
+// const c = '100500';
+
+// let w1 = 'okten';
+// let w2 = 'is';
+// let w3 = 'coll';
+// let sen = w1 + w2 + w3;
+// console.log(sen);
+//
+// let sen2 = `${w1} ${w2} ${w3}!`;
+// console.log(sen2);
+
+
+// let numToStr = 123 + 'stringa';
+// console.log(numToStr);
+//
+// //console.log(!!'asdasdasd'); - true
+// // !!('') - false
+// // < > >= <= == === != !==
+//
+// let obj1 : {age, number} = {
+//     name : Yurii;
+//     age  : 31;
+//
+// }
+// console.log(obj1);
+
+
+// let arr = [obj1, obj2];
+// console.log(arr);  masiv
+
+
+
+
